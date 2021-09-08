ef8d144d54c747b7e88a1a1dcd088f695cbb1abd
Spring Nuance <https://github.com/SpringNuance>
2021-09-09 00:36:40 +0300

    phonebook finished

---
+++ Exercise 2.15 - 2.18 The Phonebook/src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom'
+import App from './App.js'
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
+/* 
+import ReactDOM from 'react-dom'
+import App from './App.js'
+
+ReactDOM.render(<App />, document.getElementById('root'))
+*/
